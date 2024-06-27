76183df5a52b31f8fd5c2fddb3997d3bd2bbb713
Fred Silva <fredsilva015@gmail.com>
2024-06-27 19:44:01 +0100

    added basic html structure

---
+++ src/menu/Menu.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'Menu.css';
+
+function Menu() {
+  return (
+    <div>
+      <div>
+        <button></button>
+      </div>
+      <link>Link 1</link>
+      <link>Link 2</link>
+      <link>Link 3</link>
+      <link>Link 4 ver big link with a lot of stuff</link>
+      <link>Link 5</link>
+      <link>Link 4 ver big link with a lot of stuff</link>
+      <link>Link 5</link>
+      <link>Link 5</link>
+      <link>Current Link</link>
+    </div>
+  );
+}
+
+export default Menu;
