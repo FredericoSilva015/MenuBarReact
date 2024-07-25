3ecd33ca5a36dc2567b001b3087b0f9cef56e165
Fred Silva <fredsilva015@gmail.com>
2024-07-25 16:43:46 +0100

    corrected html links and syntax

--- src/menu/Menu.tsx
+++ src/menu/Menu.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import 'Menu.css';
+import './Menu.css';
 
 function Menu() {
   return (
-    <div>
-      <div>
-        <button></button>
+    <section className="menu-box">
+      <div className="menu-button">
+        <button>toggle close/open</button>
       </div>
-      <link>Link 1</link>
-      <link>Link 2</link>
-      <link>Link 3</link>
-      <link>Link 4 ver big link with a lot of stuff</link>
-      <link>Link 5</link>
-      <link>Link 4 ver big link with a lot of stuff</link>
-      <link>Link 5</link>
-      <link>Link 5</link>
-      <link>Current Link</link>
-    </div>
+      <div className="menu-box-links">
+        <a href="google.com" className="menu-links">
+          Link 1
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 2
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 3
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 4 ver big link with a lot of stuff
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 5
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 4 ver big link with a lot of stuff
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 5
+        </a>
+        <a href="google.com" className="menu-links">
+          Link 5
+        </a>
+        <a href="google.com" className="menu-links">
+          Current Link
+        </a>
+      </div>
+    </section>
   );
 }
 
